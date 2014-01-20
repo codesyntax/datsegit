f7e221a42bd19178bdcd6bac166cd7313e65b593
Aitzol Naberan <anaberan@codesyntax.com>
2014-01-20 15:08:16 +0100

    Basic urls setup

--- datsegit/urls.py
+++ datsegit/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import DetailView
+from twhst.models import Hashtag
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'datsegit.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    #url(r'^(P<hashtag>[^/]*)/(P<detail>[^/]*)', 'datsegit.views.hashtagDetail', name='hashtag_detail'),
+    url(r'^(?P<slug>[^/]*)$', DetailView.as_view(model=Hashtag, slug_field='name'), name='hashtag_index'),
     url(r'^admin/', include(admin.site.urls)),
 )
