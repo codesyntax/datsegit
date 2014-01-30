ed882f51a0cb989eb707f1d64d5e7feada18b718
Aitzol Naberan <anaberan@codesyntax.com>
2014-01-30 17:50:29 +0100

    Add alphabetical and by word filters

--- datsegit/urls.py
+++ datsegit/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import DetailView, ListView
 from twhst.models import Hashtag
+from datsegit.views import StatusFilteredList
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,7 +11,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'datsegit.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     #url(r'^(P<hashtag>[^/]*)/(P<detail>[^/]*)', 'datsegit.views.hashtagDetail', name='hashtag_detail'),
-    url(r'^$', ListView.as_view(model=Hashtag, template_name="index.html"), name="homepage"),               
+    url(r'^$', ListView.as_view(model=Hashtag, template_name="index.html"), name="homepage"),
+    url(r'^(?P<slug>[^/]*)/(?P<letter>[a-z]+)', StatusFilteredList.as_view(), name='hashtag_letter'),
     url(r'^(?P<slug>[^/]*)$', DetailView.as_view(model=Hashtag, slug_field='name'), name='hashtag_index'),
     url(r'^admin/', include(admin.site.urls)),
 )
