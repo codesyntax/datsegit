b6c22d76ea9a91babac7cb8e7c43adc8e9f0d180
csmant <csmant@anboto>
2014-01-30 13:09:30 +0100

    Frontpage url

--- datsegit/urls.py
+++ datsegit/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from twhst.models import Hashtag
 
 from django.contrib import admin
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'datsegit.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     #url(r'^(P<hashtag>[^/]*)/(P<detail>[^/]*)', 'datsegit.views.hashtagDetail', name='hashtag_detail'),
+    url(r'^$', ListView.as_view(model=Hashtag, template_name="index.html"), name="homepage"),               
     url(r'^(?P<slug>[^/]*)$', DetailView.as_view(model=Hashtag, slug_field='name'), name='hashtag_index'),
     url(r'^admin/', include(admin.site.urls)),
 )
