76bc713339e61fc5ba78aa550a440916f94f5198
Aitzol Naberan <anaberan@codesyntax.com>
2014-01-31 13:37:36 +0100

    Case insensitive filtering

--- datsegit/urls.py
+++ datsegit/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     #url(r'^(P<hashtag>[^/]*)/(P<detail>[^/]*)', 'datsegit.views.hashtagDetail', name='hashtag_detail'),
     url(r'^$', ListView.as_view(model=Hashtag, template_name="index.html"), name="homepage"),
-    url(r'^(?P<slug>[^/]*)/(?P<letter>[a-z]+)', StatusFilteredList.as_view(), name='hashtag_letter'),
+    url(r'^(?P<slug>[^/]*)/(?P<letter>[a-zA-Z]+)', StatusFilteredList.as_view(), name='hashtag_letter'),
     url(r'^(?P<slug>[^/]*)$', DetailView.as_view(model=Hashtag, slug_field='name'), name='hashtag_index'),
     url(r'^admin/', include(admin.site.urls)),
 )
