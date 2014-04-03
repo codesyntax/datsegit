49355a731c48437cefd9da460985dc7612b0c077
Aitzol Naberan <anaberan@codesyntax.com>
2014-04-03 12:22:22 +0200

    Filter unique dictionary entries

--- datsegit/views.py
+++ datsegit/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.views.generic import ListView, DetailView
 from twhst.models import Status, Hashtag, Dictionary
 
@@ -5,20 +6,20 @@ class StatusFilteredList(ListView):
     def get_context_data(self, **kwargs):
         context = super(StatusFilteredList, self).get_context_data(**kwargs)
         context['object'] = Hashtag.objects.get(name=self.kwargs.get('slug'))
-        context['dictionary'] = Dictionary.objects.filter(slug__startswith=self.kwargs.get('letter'), hashtag=context['object']).order_by('title')
+        context['dictionary'] = Dictionary.objects.filter(slug__startswith=self.kwargs.get('letter'), hashtag=context['object']).values('title', 'slug').annotate(Count('slug')).order_by('title')
         return context
     
     def get_queryset(self):
         self.hashtag = Hashtag.objects.get(name=self.kwargs.get('slug'))
         self.filter = self.kwargs.get('letter')
-        return Status.objects.filter(hashtag=self.hashtag, text__istartswith=self.filter).order_by('text')[:3]
+        return Status.objects.filter(hashtag=self.hashtag, text__istartswith=self.filter).order_by('text')[:1]
 
 class HashtagDetailView(DetailView):
     model=Hashtag
     def get_context_data(self, **kwargs):
         context = super(HashtagDetailView, self).get_context_data(**kwargs)
         hashtag = Hashtag.objects.get(name=self.kwargs.get('slug'))        
-        context['dictionary'] = Dictionary.objects.filter(hashtag=hashtag).order_by('-added')[:30]
+        context['dictionary'] = Dictionary.objects.filter(hashtag=hashtag).values('title', 'slug').annotate(Count('slug')).order_by('-added')[:30]
         return context
 
 class DictionaryListView(ListView):
